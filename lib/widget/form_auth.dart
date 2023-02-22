4eb58ea31ef20a1fd66697fc82b4e1e5179b82c0
TahsinulAmir <tahsinulamir1@gmail.com>
2023-02-22 21:56:18 +0700

    Auth Screen: add Form Login

--- lib/widget/form_auth.dart
+++ lib/widget/form_auth.dart
@@ -7,7 +7,48 @@ class FormAuth extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Card(
-        child: Text("data"),
+        margin: EdgeInsets.symmetric(vertical: 200, horizontal: 20),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      label: Text("Username"),
+                    ),
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      label: Text("Email"),
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    // keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                      label: Text("Password"),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Login"),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text("Create Account"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
