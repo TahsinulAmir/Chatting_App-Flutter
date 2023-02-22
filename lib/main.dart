a697880a2212a60cbe1aac4dd7ee6034c5936cd0
TahsinulAmir <tahsinulamir1@gmail.com>
2023-02-22 14:23:39 +0700

    Konfigurasi Firebase: initialize Firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:chatting_app/screen/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
+import 'firebase_options.dart';
+
+void main(List<String> args) async {
+  // membaca firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  // initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
