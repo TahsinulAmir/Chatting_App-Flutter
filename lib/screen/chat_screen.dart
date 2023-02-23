4c36b9629f23266dd08f01051883b01cdfd42db0
TahsinulAmir <tahsinulamir1@gmail.com>
2023-02-23 20:52:01 +0700

    Input Text: cut to message.dart & create Column in chat_screen.dart

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chatting_app/widget/message.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -15,52 +16,35 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection('chats/B8x1tzwNzIEW2WVF404v/messages')
-              // menambahkan data & dibuat mapping
-              .add({'text': "New Data"});
-        },
-        child: Icon(Icons.add),
-      ),
-      appBar: AppBar(
-        title: Text("Chatting App"),
-        actions: [
-          IconButton(
-            onPressed: () {
-              // Untuk log out
-              FirebaseAuth.instance.signOut();
-            },
-            icon: Icon(Icons.exit_to_app),
-          ),
-        ],
-      ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('chat').snapshots(),
-        builder: (context, snapshot) {
-          // jika snapshot aktif
-          if (snapshot.connectionState == ConnectionState.active) {
-            // mengambil data chats
-            final chatList = snapshot.data!.docs;
-            // maka..
-            return ListView.builder(
-              itemCount: chatList.length,
-              itemBuilder: (context, index) {
-                // get chat
-                final chat = chatList[index];
-                return ListTile(
-                  title: Text(chat['text']),
-                );
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            FirebaseFirestore.instance
+                .collection('chats/B8x1tzwNzIEW2WVF404v/messages')
+                // menambahkan data & dibuat mapping
+                .add({'text': "New Data"});
+          },
+          child: Icon(Icons.add),
+        ),
+        appBar: AppBar(
+          title: Text("Chatting App"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                // Untuk log out
+                FirebaseAuth.instance.signOut();
               },
-            );
-          } else {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      ),
-    );
+              icon: Icon(Icons.exit_to_app),
+            ),
+          ],
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Expanded(
+                child: Messages(),
+              ),
+            ],
+          ),
+        ));
   }
 }
--- lib/widget/message.dart
+++ lib/widget/message.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -6,6 +8,30 @@ class Messages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('chat').snapshots(),
+      builder: (context, snapshot) {
+        // jika snapshot aktif
+        if (snapshot.connectionState == ConnectionState.active) {
+          // mengambil data chats
+          final chatList = snapshot.data!.docs;
+          // maka..
+          return ListView.builder(
+            itemCount: chatList.length,
+            itemBuilder: (context, index) {
+              // get chat
+              final chat = chatList[index];
+              return ListTile(
+                title: Text(chat['text']),
+              );
+            },
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
   }
 }
